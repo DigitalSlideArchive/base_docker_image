d99c6ebf512524673864b8a514e0e106fe5088aa
David Manthey <david.manthey@kitware.com>
2017-03-24 09:43:42 -0400

    Update to OpenJPEG 2.1.2
    Also, switch to Ubuntu 16.04.
    
    Fix the travis build.  It was pinned to a specific branch.

--- testITKOpenSlide.py
+++ testITKOpenSlide.py
@@ -14,20 +14,15 @@ class TestITKOpenSlide(unittest.TestCase):
                        '/test/%s' % outputImg, '2']
         host = self.docker_client.create_host_config(
             binds=['%s/test:/test' % cwd])
-        container = \
-            self.docker_client\
-                .create_container(image,
-                                  volumes=['/test'],
-                                  host_config=host,
-                                  entrypoint=testEntryPoint,
-                                  command=testCommand
-                                  )
+        container = self.docker_client.create_container(
+            image, volumes=['/test'], host_config=host,
+            entrypoint=testEntryPoint, command=testCommand)
         self.docker_client.start(container=container.get('Id'))
         logs = self.docker_client.logs(container=container.get('Id'),
                                        stdout=True, stderr=True, stream=True)
         ret_code = self.docker_client.wait(container=container.get('Id'))
 
-        log_msg = "".join(logs)
+        log_msg = ''.join(logs)
         testEntryPoint.extend(testCommand)
 
         error = self.getErrMsg(image, testEntryPoint)
@@ -35,8 +30,8 @@ class TestITKOpenSlide(unittest.TestCase):
         self.assertEqual(ret_code, 0, error + log_msg)
 
     def getErrMsg(self, image, args):
-        return 'Docker image %s failed to execute the following' \
-               ' command ' % image + ' '.join(args) + '\n' * 2
+        return 'Docker image %s failed to execute the following ' \
+               'command ' % image + ' '.join(args) + '\n' * 2
 
     def setUp(self):
         # version is set to auto so the client version
@@ -46,8 +41,8 @@ class TestITKOpenSlide(unittest.TestCase):
 
     def test_tiff(self):
         imgName = '27.tiff'
-        imgLoc = 'https://data.kitware.com/api/v1/file' \
-                 '/57a35fa38d777f126827a710/download'
+        imgLoc = ('https://data.kitware.com/api/v1/file/'
+                  '58d526458d777f0aef5d8935/download')
         cwd = os.getcwd()
         wget.download(imgLoc, out=cwd + '/test', bar=None)
         self.OpenSlideITKRead('dsarchive/base_docker_image', imgName,
@@ -55,10 +50,10 @@ class TestITKOpenSlide(unittest.TestCase):
         self.checkType('out1.jpg', 'ffd8ffe0')
 
     def test_svs(self):
-        imgName = 'TCGA-CV-7242-11A-01-TS1.1838afb1-9eee-4a' \
-                  '70-9ae3-50e3ab45e242.svs'
-        imgLoc = 'https://data.kitware.com/api/v1/file/' \
-                 '57a3519e8d777f126827a679/download'
+        imgName = ('TCGA-CV-7242-11A-01-TS1.1838afb1-9eee-4a70-9ae3-'
+                   '50e3ab45e242.svs')
+        imgLoc = ('https://data.kitware.com/api/v1/file/'
+                  '58d525f88d777f0aef5d8933/download')
         cwd = os.getcwd()
         wget.download(imgLoc, out=cwd + '/test', bar=None)
         self.OpenSlideITKRead('dsarchive/base_docker_image', imgName,
@@ -68,10 +63,10 @@ class TestITKOpenSlide(unittest.TestCase):
     def checkType(self, imgName, header):
         imgFile = open('test/' + imgName)
         img = imgFile.read(4)
-        self.assertIsInstance(img, six.binary_type,
-                              "image %s is not being read as a"
-                              " bytes object" % imgName)
-        img = ''.join("{:02x}".format(ord(imgByte)) for imgByte in img)
+        self.assertIsInstance(
+            img, six.binary_type,
+            'image %s is not being read as a bytes object' % imgName)
+        img = ''.join('{:02x}'.format(ord(imgByte)) for imgByte in img)
         self.assertEqual(img[:], header,
                          'First 4 bytes of the %s image is %s' % (
                          imgName, img[:]))
